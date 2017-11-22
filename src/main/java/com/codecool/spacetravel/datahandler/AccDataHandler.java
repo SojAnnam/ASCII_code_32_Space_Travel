33e8733759b5b8ba2d906e164946acdc2fa65ef3
Molnar Marton <mmolnr11@gmail.com>
2017-11-22 15:00:36 +0100

    adding AccHandlertoDev

---
+++ src/main/java/com/codecool/spacetravel/datahandler/AccDataHandler.java
@@ -0,0 +1,42 @@
+package com.codecool.spacetravel.datahandler;
+
+import com.codecool.spacetravel.model.Accomodation;
+import com.codecool.spacetravel.model.Planet;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class AccDataHandler {
+
+    private EntityManager em;
+    public AccDataHandler(EntityManager em) {
+        this.em = em;
+    }
+
+    public EntityManager getEm() {
+        return em;
+    }
+
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+
+    public  List getAccByPlanetId(long id){
+
+        List<Accomodation> results = em.createNamedQuery("getAccByPlanetId", Accomodation.class)
+                .setParameter("planetId", id).getResultList();
+        System.out.println("SIZE IN QueryController: " + results.size());
+
+        return results;
+    }
+
+    public Planet getPlanet(long planetId){
+
+        Planet planet = em.createNamedQuery("getPlanet", Planet.class)
+                .setParameter("planetId", planetId).getSingleResult();
+
+        return planet;
+    }
+
+}
+
