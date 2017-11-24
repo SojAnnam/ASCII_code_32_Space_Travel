2ba5d449afea5cf12b25e023c4dad1f8e7a80677
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-11-24 09:26:48 +0100

    add donothing mocking (test inprogress)

--- src/test/java/com/codecool/spacetravel/datahandler/RoomDataHandlerTest.java
+++ src/test/java/com/codecool/spacetravel/datahandler/RoomDataHandlerTest.java
@@ -9,11 +9,9 @@ import org.mockito.Mockito;
 import java.util.*;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RoomDataHandlerTest {
     @Test
@@ -29,6 +27,7 @@ class RoomDataHandlerTest {
         roomReservationDatas.put("endDateStringFromUser", "2019/01/02");
         List<String> exampleList = new ArrayList<>();
         Customer costumer = new Customer();
+        
 
         QueryHandler QueryHandlerMock = Mockito.mock(QueryHandler.class);
         RoomReservationDataValidator roomReservationMock = Mockito.mock(RoomReservationDataValidator.class);
@@ -39,7 +38,7 @@ class RoomDataHandlerTest {
 
         when(QueryHandlerMock.getCustomerById(1L)).thenReturn(costumer);
         when(roomDataHandler1.roomIsFree(new Room(),new Date(2018,01,02),new Date(2019,01,02))).thenReturn(true);
-        //verify(QueryHandlerMock ,Mockito.times(1)).persistData(any());
+        doNothing().when(QueryHandlerMock).persistData();
         boolean actual = roomDataHandler.saveRoomReservation(roomReservationDatas,exampleList);
         assertTrue(actual);
     }
