271b75d956dcbcec31006c38cf38016b4b4995a6
krisztian <alt.krisztian@gmail.com>
2017-12-02 23:32:05 +0100

    Create AccomodationPicture class

--- src/main/java/com/codecool/spacetravel/model/AccomodationPicture.java
+++ src/main/java/com/codecool/spacetravel/model/AccomodationPicture.java
@@ -1,4 +1,71 @@
 package com.codecool.spacetravel.model;
 
+import javax.persistence.*;
+
+@Entity
 public class AccomodationPicture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String fileName;
+
+    private String description;
+
+    private String title;
+
+    @ManyToOne
+    private Accomodation accomodation;
+
+    public AccomodationPicture() {
+    }
+
+    public AccomodationPicture(String fileName, String description, String title) {
+
+        this.fileName = fileName;
+        this.description = description;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Accomodation getAccomodation() {
+        return accomodation;
+    }
+
+    public void setAccomodation(Accomodation accomodation) {
+        this.accomodation = accomodation;
+    }
+
 }
