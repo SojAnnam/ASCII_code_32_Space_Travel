182bbfd1fae59f963777e379ed72e83bdf1ce508
krisztian <alt.krisztian@gmail.com>
2017-12-04 18:04:45 +0100

    Rewrite main() method to Spring

--- src/main/java/com/codecool/spacetravel/SpaceTravelAgency.java
+++ src/main/java/com/codecool/spacetravel/SpaceTravelAgency.java
@@ -1,25 +1,15 @@
 package com.codecool.spacetravel;
 
-import static spark.Spark.*;
-import static spark.debug.DebugScreen.enableDebugScreen;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpaceTravelAgency {
 
     public static void main(String[] args) {
 
-        // default server settings
-        exception(Exception.class, (e, req, res) -> e.printStackTrace());
-        staticFileLocation("/public");
-        port(8888);
-
-        // Add this line to your project to enable the debug screen
-        enableDebugScreen();
-
         System.out.println("Starting...");
-
-        DIContainer diContainer = new DIContainer();
-        Routes routes = new Routes(diContainer);
-        routes.initRoutes();
+        SpringApplication.run(SpaceTravelAgency.class, args);
 
     }
 
