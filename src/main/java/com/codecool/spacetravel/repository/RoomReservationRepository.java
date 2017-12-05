f1728e4115027c0f4a040877a79f3162fa37e021
krisztian <alt.krisztian@gmail.com>
2017-12-05 22:55:25 +0100

    Implement named query calling

--- src/main/java/com/codecool/spacetravel/repository/RoomReservationRepository.java
+++ src/main/java/com/codecool/spacetravel/repository/RoomReservationRepository.java
@@ -2,6 +2,12 @@ package com.codecool.spacetravel.repository;
 
 import com.codecool.spacetravel.model.RoomReservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RoomReservationRepository extends JpaRepository<RoomReservation, Long> {
+
+    List<RoomReservation> getRoomReservationsByCustomerId(@Param("customerId") Long customerId);
+
 }
