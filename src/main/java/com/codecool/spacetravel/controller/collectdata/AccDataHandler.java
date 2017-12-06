a733c919d3a47d4bb2087b847c74ba26fd5a3da7
bencePal <paal.bence@gmail.com>
2017-12-06 15:01:58 +0100

    collect data for accommodation registration

--- src/main/java/com/codecool/spacetravel/controller/collectdata/AccDataHandler.java
+++ src/main/java/com/codecool/spacetravel/controller/collectdata/AccDataHandler.java
@@ -2,24 +2,24 @@ package com.codecool.spacetravel.controller.collectdata;
 
 import com.codecool.spacetravel.DAO.QueryHandler;
 import com.codecool.spacetravel.model.Accomodation;
+import com.codecool.spacetravel.model.AmenityType;
 import com.codecool.spacetravel.model.Planet;
+import com.codecool.spacetravel.repository.AmenityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class AccDataHandler {
 
+    @Autowired
     private QueryHandler queryHandler;
-
-    public AccDataHandler(QueryHandler queryHandler) {
-
-        this.queryHandler = queryHandler;
-    }
 
     public void collectAccData(Model model,
                                 long planetId,
@@ -31,6 +31,10 @@ public class AccDataHandler {
         Planet planet = queryHandler.getPlanet(planetId);
 
         List<Accomodation> accList = queryHandler.getAccByPlanetId(planetId);
+        for (Accomodation accomodation : accList
+             ) {
+            System.out.println(accomodation.getName());
+        }
 
         model.addAttribute("loggedIn", customerId != null);
         model.addAttribute("customername", customerName);
@@ -39,13 +43,16 @@ public class AccDataHandler {
 
     }
 
-    public void collectAccRegistrationData(//@RequestParam Map<String,String> allRequestParams,
-                                                     Model model,
-                                                     HttpServletRequest httpServletRequest) {
-
+    public void collectAccRegistrationData(Model model, HttpServletRequest httpServletRequest) {
         Integer userId = (Integer) httpServletRequest.getSession().getAttribute("user_id");
+        List<Planet> allPlanet = queryHandler.getAllPlanet();
+        List<AmenityType> amenityTypes = queryHandler.getAllAmenity();
 
         model.addAttribute("loggedIn", userId != null);
+        model.addAttribute("planets", allPlanet);
+        model.addAttribute("amenityTypes", amenityTypes);
+        model.addAttribute("accomodation", new Accomodation());
+
     }
 
 }
