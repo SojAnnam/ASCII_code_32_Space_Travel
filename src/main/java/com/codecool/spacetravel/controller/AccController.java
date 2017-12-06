6aa171e0a7581578f6aa38db36ac3919cac94d5b
bencePal <paal.bence@gmail.com>
2017-12-06 14:59:40 +0100

    added register accommodation route with GET/POST

--- src/main/java/com/codecool/spacetravel/controller/AccController.java
+++ src/main/java/com/codecool/spacetravel/controller/AccController.java
@@ -1,20 +1,24 @@
 package com.codecool.spacetravel.controller;
 
+import com.codecool.spacetravel.DAO.QueryHandler;
 import com.codecool.spacetravel.controller.collectdata.AccDataHandler;
+import com.codecool.spacetravel.model.Accomodation;
+import com.codecool.spacetravel.model.AmenityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Controller
 public class AccController {
 
     @Autowired
     private AccDataHandler accDataHandler;
+    @Autowired
+    private QueryHandler queryHandler;
 
     @RequestMapping(value = "/{planetId}/accomodation", method = RequestMethod.GET)
     public String renderAcc(Model model,
@@ -25,12 +29,16 @@ public class AccController {
     }
 
     @RequestMapping(value = "/registration-accommodation", method = RequestMethod.GET)
-    public String renderAcc(Model model,
-            HttpServletRequest httpServletRequest) {
+    public String renderAcc(Model model, HttpServletRequest httpServletRequest) {
         accDataHandler.collectAccRegistrationData(model, httpServletRequest);
         return "registration_accommodation";
     }
 
+    @RequestMapping(value = "/registration-accommodation", method = RequestMethod.POST)
+    public String getAcc(Accomodation accomodation) {
+        queryHandler.saveAccommodation(accomodation);
+        return "redirect:/registration-accommodation";
+    }
 
 
 }
