f300f0771eb241f7982c3eb044ca27554ee0086a
Molnar Marton <mmolnr11@gmail.com>
2017-12-06 11:29:56 +0100

    create amenity data handler class

---
+++ src/main/java/com/codecool/spacetravel/controller/collectdata/AmenityDataHandler.java
@@ -0,0 +1,14 @@
+package com.codecool.spacetravel.controller.collectdata;
+
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Service
+public class AmenityDataHandler {
+
+    public void collectAmenityData(Map<String, String> allRequestParams, String accomodationId, Model model, HttpServletRequest httpServletRequest) {
+    }
+}
